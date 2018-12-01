3a6103ca2a0121107f747731c8774e5ef21bd94f
iortizfe <iortizfe@fp.uoc.edu>
2018-12-01 20:54:37 +0100

    Home v0.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,68 +1,57 @@
-$(document).ready(function(){
-    responsiveMenu();
-});
+(function(){
+  $('#m-burger-menu').on('click', function(){
+    $('.m-menu--secondary').toggleClass('active');
+  });
+})();
 
-function responsiveMenu(){
-    $('.has-children').hover(function(){
-        $(this).find('.submenu').addClass('showsub');
-    },function(){
-        $(this).find('.submenu').removeClass('showsub');
+(function(){
+    $('#o-hero--home').vide('http://localhost/movon/assets/video/movon.mp4', {
+        volume: 1,
+        playbackRate: 1,
+        muted: true,
+        loop: true,
+        autoplay: true,
+        position: '50% 50%',
+        posterType: 'none',
+        bgColor: '#000',
+        resizing: true, 
     });
+})();
 
-    $('#m-burger-menu').on('click', function(){
-      $('.m-menu--secondary').addClass('active');
-    });
-
-    $('#m-burger-menu--close').on('click', function(){
-      $('.m-menu--secondary').removeClass('active');
-    });
-
-    $('.dropdown-button').hover(function(){
-      console.log("Entra");
-      $(this).find('.m-dropdown').addClass('active');
-    }, function(){
-      console.log("Sale");
-      $(this).find('.m-dropdown').removeClass('active');
-    });
-  }
-}
-
-
-function scrollTo() {
-    // Select all links with hashes
-    $('a[href*="#"]')
-      // Remove links that don't actually link to anything
-      .not('[href="#"]')
-      .not('[href="#0"]')
-      .click(function(event) {
-        // On-page links
-        if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-          && 
-          location.hostname == this.hostname
-        ) {
-          // Figure out element to scroll to
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          // Does a scroll target exist?
-          if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000, function() {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              };
-            });
+(function(){
+      var el = document.getElementById('o-hero--home');
+			if(el !== null){
+          var words;
+          if(el.classList.contains('english')){
+            words = new Array("to feel it?","to live it?","to enjoy it?");
+          }else if(el.classList.contains('spanish')){
+            words = new Array("sentirlo?","vivirlo?","disfrutarlo?");
           }
-        }
-    });
-}
+
+          var sortWords = words.sort(function(a, b){return 0.5 - Math.random()});
+          var catchWord = document.getElementById("catch");
+          goText(0);
+      }
+
+
+			function showText(word, text, posLetra, posPalabra){
+				text += word[posLetra];
+				catchWord.innerHTML = text;
+				posLetra++;
+
+				if(posLetra<word.length){	
+					setTimeout(function(){showText(word,text,posLetra,posPalabra);},170);
+				}else{
+					posPalabra++;
+					if(posPalabra == sortWords.length){posPalabra = 0;};
+					setTimeout(function(){goText(posPalabra);},1500);
+				}
+			}
+
+			function goText(posPalabra){
+				var word = sortWords[posPalabra].split("");
+				var j = 0;
+				var text = "";
+				setTimeout(function(){showText(word,text,j,posPalabra);},700);
+			}
+})();
