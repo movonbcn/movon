5106bcaaf8139275a7b830ee5a8caf54c8b04052
iortizfe <iortizfe@fp.uoc.edu>
2018-12-06 18:18:03 +0100

    Travels / Prices

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,15 @@
 (function(){
-  $('#m-burger-menu').on('click', function(){
-    $('.m-menu--secondary').toggleClass('active');
-  });
+    $('#m-menu--burger').on('click', function(){
+        $(this).toggleClass('menu-open');
+        $('header').toggleClass('menu-open');
+        $('.m-menu--mobile--bg').toggleClass('menu-open');
+    });
+
+    $('.m-menu--mobile--bg').on('click', function(){
+        $(this).removeClass('menu-open');
+        $('header').removeClass('menu-open');
+        $('#m-menu--burger').removeClass('menu-open');
+    });
 })();
 
 (function(){
@@ -54,4 +62,15 @@
 				var text = "";
 				setTimeout(function(){showText(word,text,j,posPalabra);},700);
 			}
+})();
+
+(function(){
+    var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
+    var map = L.map('map').setView([41.40332,2.18152], 17).addLayer(osm);
+    L.marker([41.40332,2.18152])
+        .addTo(map)
+        .bindPopup('Movon')
+        .openPopup();
 })();
